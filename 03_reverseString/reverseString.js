c3e0583833eab6001d694b22cdfd77fabbe63dea
David Rosales <137208896+plunkydev@users.noreply.github.com>
2023-09-27 18:17:30 -0400

    Test finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(letter) {
+    let result = letter.split('');
+    return result.reverse().join('');
 };
 
 // Do not edit below this line
