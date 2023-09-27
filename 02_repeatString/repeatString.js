e3bed71d81897f7f403abbf3f04104e6b8482f35
David Rosales <137208896+plunkydev@users.noreply.github.com>
2023-09-26 20:07:26 -0400

    Test pasado

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, times) {
+    let result = '';
+      if (times < 0) {
+          return "ERROR";
+      } else if(times === 0) {
+          return '';
+      } 
+          for (let i = 0;i < times;i++) {
+              result += word;
+          }
+          return result;
+  };
 
 // Do not edit below this line
 module.exports = repeatString;
