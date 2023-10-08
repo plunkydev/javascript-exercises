0cd21980dc0541ce81dcbf22ae25a1cc1c2dd251
David Rosales <137208896+plunkydev@users.noreply.github.com>
2023-10-08 17:13:47 -0400

    sumAll.js finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(start, end) {
+    let arr = [];
+    let result = 0;
+    if(start > end) {
+      for(let i = end;i <= start;i += 1){
+      arr.push(i);
+    } 
+    } else if(start < 1) {
+      return 'ERROR';
+    } else if(typeof end !== 'number') {
+      return 'ERROR';
+    } else {
+      for(let i = start;i <= end;i += 1){
+        arr.push(i);
+    }
+    }
+    result = arr.reduce((e,i) => e + i);
+    return result;
+  };
 // Do not edit below this line
 module.exports = sumAll;
