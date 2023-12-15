8119d98a527326462a1d06674893d587a0a0ae68
plunkydev <137208896+plunkydev@users.noreply.github.com.>
2023-12-15 17:40:53 -0400

    Test calculadora terminado

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,69 @@
 const add = function() {
-	
+	let arr = [];
+  for(let ele of arguments) {
+    arr.push(Number(ele));
+  }
+  return arr.reduce((acc, el) => acc + el);
 };
 
 const subtract = function() {
-	
+	let arr = [];
+  for(let ele of arguments) {
+    arr.push(Number(ele));
+  }
+  return arr.reduce((acc, el) => acc - el);
 };
 
 const sum = function() {
-	
+	let arr = [];
+  for(let ele of arguments[0]) {
+    arr.push(Number(ele));
+  }
+  if(arr[0] === undefined) {
+    return 0;
+  }
+  return arr.reduce((acc, el) => acc + el);
 };
 
 const multiply = function() {
-
+  let arr = [];
+  for(let ele of arguments[0]) {
+    arr.push(Number(ele));
+  }
+  if(arr[0] === undefined) {
+    return 0;
+  }
+  return arr.reduce((acc, el) => acc * el);
 };
 
 const power = function() {
-	
+	let arr = [];
+  for(let ele of arguments) {
+    arr.push(Number(ele));
+  }
+  if(arr[0] === undefined) {
+    return 0;
+  }
+  let result = [];
+  for(let i = 0;i < arr[1];i++){
+    result.push(arr[0])
+  }
+  return result.reduce((acc, el) => acc * el);
 };
 
 const factorial = function() {
-	
+	let longitud;
+  for(let ele of arguments) {
+    longitud = Number(ele);
+  }
+  let result = [];
+  for(let i = 1;i <= longitud;i++){
+    result.push(i)
+  }
+  if(longitud === 0) {
+    return longitud = 1;
+  }
+  return result.reduce((acc, el) => acc * el);
 };
 
 // Do not edit below this line
