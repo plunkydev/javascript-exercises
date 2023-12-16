8f8e99834c7a49c0243825fbe2a5327e45d57567
plunkydev <137208896+plunkydev@users.noreply.github.com.>
2023-12-16 18:50:49 -0400

    Test fibonacci.js pasado

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (limit) {
+let fibo = [1, 1];
+let result = 0;
+if (limit < 1) {
+    return "OOPS";
+}
+for (let i = 2; i < limit; i++) {
+    fibo.push(fibo[i - 1] + fibo[i - 2]);
+}
+for (let i = 0; i < fibo.length; i++) {
+    result = fibo[i];
+}
+return result;
 };
 
 // Do not edit below this line
