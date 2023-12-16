3907bf99e7aabc325377383dced2ceae13dbd23f
plunkydev <137208896+plunkydev@users.noreply.github.com.>
2023-12-16 17:27:30 -0400

    Test palindrome pasado

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let a = str.toLowerCase().match(/[A-Za-z0-9]/g);
+    let b = str.toLowerCase().match(/[A-Za-z0-9]/g).reverse();
+    return a.join('') == b.join('')
 };
 
 // Do not edit below this line
